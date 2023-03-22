8b4a1e9f8898bd39e8be286efbe2cdb87efa20db
DivisionCom <xd6511@gmail.com>
2023-03-22 13:55:21 +0300

    Add function that parsing data from JSONObject

--- app/src/main/java/com/example/bin_information_cft/MainActivity.kt
+++ app/src/main/java/com/example/bin_information_cft/MainActivity.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.example.bin_information_cft.adapters.CardModel
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,13 +24,36 @@ class MainActivity : AppCompatActivity() {
         val request = StringRequest(
             Request.Method.GET,
             url,
-            {
-                    result -> Log.d("MyLog", "Result: $result")
+            { result ->
+                parseCardData(result)
             },
-            {
-                    error -> Log.d("MyLog", "Error: $error")
+            { error ->
+                Log.d("MyLog", "Error: $error")
             }
         )
         queue.add(request)
+    }
+
+    private fun parseCardData(result: String) {
+        val mainObject = JSONObject(result)
+        val item = CardModel(
+            mainObject.getJSONObject("number").getString("length"),
+            mainObject.getJSONObject("number").getString("luhn"),
+            mainObject.getString("scheme"),
+            mainObject.getString("type"),
+            mainObject.getString("brand"),
+            mainObject.getString("prepaid"),
+            mainObject.getJSONObject("country").getString("numeric"),
+            mainObject.getJSONObject("country").getString("alpha2"),
+            mainObject.getJSONObject("country").getString("name"),
+            mainObject.getJSONObject("country").getString("emoji"),
+            mainObject.getJSONObject("country").getString("currency"),
+            mainObject.getJSONObject("country").getString("latitude"),
+            mainObject.getJSONObject("country").getString("longitude"),
+            mainObject.getJSONObject("bank").getString("name"),
+            mainObject.getJSONObject("bank").getString("url"),
+            mainObject.getJSONObject("bank").getString("phone"),
+            mainObject.getJSONObject("bank").getString("city"),
+        )
     }
 }

6bfef20432081ccf9058cd5926d57070bc9f263c
DivisionCom <xd6511@gmail.com>
2023-03-22 13:48:55 +0300

    Add function that getting data via API

--- app/src/main/java/com/example/bin_information_cft/MainActivity.kt
+++ app/src/main/java/com/example/bin_information_cft/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.bin_information_cft
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        requestCardData("45717360")
+    }
+
+    private fun requestCardData(bin: String) {
+        val url = "https://lookup.binlist.net/" +
+                bin
+        val queue = Volley.newRequestQueue(this)
+        val request = StringRequest(
+            Request.Method.GET,
+            url,
+            {
+                    result -> Log.d("MyLog", "Result: $result")
+            },
+            {
+                    error -> Log.d("MyLog", "Error: $error")
+            }
+        )
+        queue.add(request)
     }
 }

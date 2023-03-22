9edb4106c076989b494f1e6fb2b0b27507089400
DivisionCom <xd6511@gmail.com>
2023-03-22 16:23:07 +0300

    Add TextChangedListener for edit

--- app/src/main/java/com/example/bin_information_cft/MainActivity.kt
+++ app/src/main/java/com/example/bin_information_cft/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.bin_information_cft
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
@@ -20,7 +23,23 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        requestCardData("45717360")
+        binding.etInput.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                requestCardData(s.toString())
+            }
+        })
     }
 
     private fun requestCardData(bin: String) {
@@ -61,6 +80,7 @@ class MainActivity : AppCompatActivity() {
             getCardData(mainObject, "bank", "phone", true),
             getCardData(mainObject, "bank", "city", true),
         )
+        binding.tvTextCountry.text = item.countryName
     }
 
     private fun getCardData(
@@ -69,36 +89,43 @@ class MainActivity : AppCompatActivity() {
         jsonString: String,
         getJSONobj: Boolean
     ): String {
-        val validatedData: String
-        when (getJSONobj) {
-            true -> {
-                validatedData = when {
-                    mainObject.has(jsonObject) -> {
-                        when {
-                            mainObject.getJSONObject(jsonObject).has(jsonString) -> {
-                                mainObject.getJSONObject(jsonObject).getString(jsonString)
-                            }
-                            else -> {
-                                ""
+        var validatedData: String
+        try {
+            when (getJSONobj) {
+                true -> {
+                    validatedData = when {
+                        mainObject.has(jsonObject) -> {
+                            when {
+                                mainObject.getJSONObject(jsonObject).has(jsonString) -> {
+                                    mainObject.getJSONObject(jsonObject).getString(jsonString)
+                                }
+                                else -> {
+                                    ""
+                                }
                             }
                         }
+                        else -> {
+                            ""
+                        }
                     }
-                    else -> {
-                        ""
+                }
+                false -> {
+                    validatedData = when {
+                        mainObject.has(jsonString) -> {
+                            mainObject.getString(jsonString)
+                        }
+                        else -> {
+                            ""
+                        }
                     }
                 }
             }
-            false -> {
-                validatedData = when {
-                    mainObject.has(jsonString) -> {
-                        mainObject.getString(jsonString)
-                    }
-                    else -> {
-                        ""
-                    }
-                }
-            }
+        } catch (ex: java.lang.Exception) {
+            validatedData = ""
+            val toast = Toast.makeText(this, ex.toString(), Toast.LENGTH_SHORT)
+            toast.show()
         }
+
         return validatedData
     }
 

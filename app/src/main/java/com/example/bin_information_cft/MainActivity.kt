7b41ae876e7f4fcb28e5493868a74ae2e168095c
DivisionCom <xd6511@gmail.com>
2023-03-22 13:36:20 +0300

    Add project files

---
+++ app/src/main/java/com/example/bin_information_cft/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bin_information_cft
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
